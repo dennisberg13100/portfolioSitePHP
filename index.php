bd3b1e6a59e61d6e0c1ba4462ff69b3a60a85859
Dennis van den Berg <dennisvandenberg13100@gmail.com>
2020-04-14 11:08:58 +0200

    Directory organization and home layout

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Dennis van den Berg</title>
+	<link rel="stylesheet" type="text/css" href="global.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+	<div class="bg">
+		<div class="face">
+			<iframe height="100%" width="100%" frameBorder="0" src="./assets/images/firstFace300.svg"></iframe>
+			<!-- <img src="./assets/images/firstFace300.svg"/> -->
+		</div>
+		<div class="container">
+			<div class="container-top">
+				<div class="icon-top">
+					teste
+				</div>
+				<div class="icon-top">
+					teste
+				</div>
+			</div>
+			<div class="container-center">
+
+				<div class="container-border-center">
+				</div>
+
+				<div class="container-border-center">
+				</div>
+				
+			</div>
+			<div class="container-center">
+
+				<div class="container-border-center">
+				</div>
+
+				<div class="container-border-center">
+				</div>
+				
+			</div>
+		</div>
+	</div>
+
+	
+		<!--  -->
+
+	<script type="text/javascript" src="./jquery.js"></script>
+	<script type="text/javascript" src="./script.js"></script>
+</body>
+</html>
